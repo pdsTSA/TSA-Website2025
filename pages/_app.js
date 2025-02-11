8612ae2e867b99b89723279ba9a36d6f04d412fc
charlieboalch <112891042+charlieboalch@users.noreply.github.com>
2025-02-11 17:56:06 -0500

    Move head component to _document.js

--- pages/_app.js
+++ pages/_app.js
@@ -4,12 +4,6 @@ import {Head} from "next/document";
 
 export default function App({ Component, pageProps }) {
   return <>
-      <Head>
-        <title>Adobo Agogo</title>
-        <meta name="description" content="Buy our food now!" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
