fafcc5c67b61575b8752cb4ecd62e2a59777525e
charlieboalch <112891042+charlieboalch@users.noreply.github.com>
2025-02-11 17:49:58 -0500

    Layout updates

---
+++ pages/catering/index.js
@@ -0,0 +1,12 @@
+import Layout from '@/components/Layout';
+
+export const index = () => {
+  return (
+      <div>
+          <p>does this work catering page???</p>
+      </div>
+  )
+}
+
+
+export default index;
---
+++ pages/menu/index.js
@@ -0,0 +1,12 @@
+import Layout from '@/components/Layout';
+
+export const index = () => {
+  return (
+      <div>
+          <p>does this work menu page???</p>
+      </div>
+  )
+}
+
+
+export default index;
