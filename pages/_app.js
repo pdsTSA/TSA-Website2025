7fc7af8d65599dfa190156eed466ff16f972053d
charlieboalch <112891042+charlieboalch@users.noreply.github.com>
2025-02-11 17:53:58 -0500

    Add site title

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,17 @@
 import "@/styles/globals.css";
 import Layout from "@/components/Layout";
+import {Head} from "next/document";
 
 export default function App({ Component, pageProps }) {
-  return <Layout>
-    <Component {...pageProps} />
-  </Layout>;
+  return <>
+      <Head>
+        <title>Adobo Agogo</title>
+        <meta name="description" content="Buy our food now!" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
 }
