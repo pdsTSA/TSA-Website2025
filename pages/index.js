0681f9b2b2f7e6f2098b212b64d66f34470e118c
charlieboalch <112891042+charlieboalch@users.noreply.github.com>
2025-02-11 19:20:26 -0500

    Add section header for home page

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,17 @@
 import styles from "./Home.module.css"
 import SplashImage from "@/components/home/splash/Splash";
+import SectionHeader from "@/components/home/section_header/SectionHeader";
+import Image from 'next/image'
+
 
 const Home = () => {
   return <div className={styles.home}>
     <SplashImage></SplashImage>
-    <div className={styles.section_header}>
-      <div><h1>Hello</h1></div>
-    </div>
+    <br></br>
+    <br></br>
+    <SectionHeader content={"Chef's Vision"}></SectionHeader>
+    <br></br>
+
   </div>
 }
 
