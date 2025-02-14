9f1d0a20829090e3e5c0c360bcd18d6d9f63de69
charlieboalch <112891042+charlieboalch@users.noreply.github.com>
2025-02-14 18:33:14 -0500

    Update navbar with colors and fonts

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,12 +1,12 @@
 import Footer from "./Footer"
 import Navbar from "./Navbar"
-import { Playfair_Display } from "next/font/google";
+import { Montserrat } from "next/font/google";
 
-const playfair = Playfair_Display({ subsets: ['latin'], weight: ['800', '500'] });
+const font = Montserrat({ subsets: ['latin'], weight: ['800', '500'] });
 
 export const Layout = ({children}) => {
   return (
-    <div className = {playfair.className}>
+    <div className = {font.className}>
         <Navbar />
         <main> {children} </main>
         <Footer />
