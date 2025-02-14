b953959deba56b18c72f84f5d7bd745527b8823b
charlieboalch <112891042+charlieboalch@users.noreply.github.com>
2025-02-14 10:58:10 -0500

    Store sign up values as cookies

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,18 @@
 import "@/styles/globals.css";
 import Layout from "@/components/layout/Layout";
 import {GoogleOAuthProvider} from "@react-oauth/google";
+import {CookiesProvider} from "react-cookie";
 
 
 
 export default function App({ Component, pageProps }) {
   return <>
-      <GoogleOAuthProvider clientId={"438612680317-5i0cdcim14q08de8b1gt2norhgfskjnp.apps.googleusercontent.com"}>
-          <Layout>
-              <Component {...pageProps} />
-          </Layout>
-      </GoogleOAuthProvider>
+      <CookiesProvider defaultSetOptions={{path: "/"}}>
+          <GoogleOAuthProvider clientId={"438612680317-5i0cdcim14q08de8b1gt2norhgfskjnp.apps.googleusercontent.com"}>
+              <Layout>
+                  <Component {...pageProps} />
+              </Layout>
+          </GoogleOAuthProvider>
+      </CookiesProvider>
     </>
 }
--- pages/signup/index.js
+++ pages/signup/index.js
@@ -1,8 +1,11 @@
 import style from "./Signup.module.css"
-import {GoogleLogin} from "@react-oauth/google";
+import {CredentialResponse, GoogleLogin} from "@react-oauth/google";
 import {Londrina_Solid} from "next/font/google";
 import Logo from "@/assets/logo.svg";
 import Link from "next/link";
+import {useCookies} from "react-cookie";
+import {jwtDecode} from "jwt-decode";
+import {useState} from "react";
 
 
 const font = Londrina_Solid({
@@ -10,35 +13,65 @@ const font = Londrina_Solid({
     weight: "400"
 })
 
+
 const index = () => {
-  return (
-      <div className={style.overflow_background}>
-          <div className={style.signup}>
-              <div className={style.signup_content}>
-                  <div className={style.signup_item}>
-                      <Logo/>
-                  </div>
-                  <h1 className={font.className}>Sign Up For Adobo Agogo</h1>
-                  <div className={style.signup_item}>
-                      <input className={font.className} placeholder={"Name"}/>
-                  </div>
-                  <div className={style.signup_item}>
-                      <input className={font.className} placeholder={"Email"}/>
-                  </div>
-                  <div className={style.signup_item}>
-                      <button className={font.className}>Sign Up!</button>
-                  </div>
-                  <h2 className={font.className}>Or log in with Google</h2>
-                  <div className={style.signup_item}>
-                      <GoogleLogin onSuccess={() => {}} />
-                  </div>
-                  <div className={style.signup_legal}>
-                      <Link href={"/terms"}>Terms of Service</Link> | <Link href={"/privacy"}>Privacy Policy</Link>
-                  </div>
-              </div>
-          </div>
-      </div>
-  )
+    const [nameCookie, setNameCookie, removeName] = useCookies(["user.name"]);
+    const [emailCookie, setEmailCookie, removeEmail] = useCookies(["user.email"]);
+    const [pictureCookie, setPictureCookie, removePicture] = useCookies(["user.picture"]);
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+
+    const parseGoogleResponse = (credentialResponse) => {
+        const cred = credentialResponse.credential;
+        const decoded = jwtDecode(cred);
+
+        setCookies(decoded.given_name, decoded.email, decoded.picture)
+    }
+
+    const setCookies = (name, email, picture) => {
+        setNameCookie("user.name", name);
+        setEmailCookie("user.email", email);
+        setPictureCookie("user.picture", picture);
+    }
+
+    const handleSubmission = () => {
+        if (name === "" || email === "") return;
+        setCookies(name, email, "default");
+    }
+
+    return (
+        <div className={style.overflow_background}>
+            <div className={style.signup}>
+                <div className={style.signup_content}>
+                    <div className={style.signup_item}>
+                        <Logo/>
+                    </div>
+                    <h1 className={font.className}>Sign Up For Adobo Agogo</h1>
+                    <div className={style.signup_item}>
+                        <input className={font.className}
+                               placeholder={"Name"}
+                               onChange={(e) => setName(e.target.value)}/>
+                    </div>
+                    <div className={style.signup_item}>
+                        <input className={font.className}
+                               placeholder={"Email"}
+                               onChange={(e) => setEmail(e.target.value)}/>
+                    </div>
+                    <div className={style.signup_item}>
+                        <button className={font.className} onClick={handleSubmission}>Sign Up!</button>
+                    </div>
+                    <h2 className={font.className}>Or log in with Google</h2>
+                    <div className={style.signup_item}>
+                        <GoogleLogin onSuccess={parseGoogleResponse}/>
+                    </div>
+                    <div className={style.signup_legal}>
+                        <Link href={"/terms"}>Terms of Service</Link> | <Link href={"/privacy"}>Privacy Policy</Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 
