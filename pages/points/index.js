560a52b9f1d9cce9f11e31bd74297faceb8e7ac4
charlieboalch <112891042+charlieboalch@users.noreply.github.com>
2025-02-14 19:29:38 -0500

    Tweaks to points

--- pages/points/index.js
+++ pages/points/index.js
@@ -5,6 +5,7 @@ import styles from "./Points.module.css"
 import {Londrina_Solid} from "next/font/google";
 import Image from "next/image";
 import {useEffect, useState} from "react";
+import {useRouter} from "next/navigation";
 
 const font = Londrina_Solid({
     subsets: ['latin'],
@@ -29,10 +30,21 @@ const Points = () => {
         setName((cookies["user.name"] === "") ? null : cookies["user.name"]);
     }, [cookies]);
 
+    const router = useRouter();
+
+    const logOut = () => {
+        router.push("/")
+
+        removeCookie("user.email")
+        removeCookie("user.name")
+        removeCookie("user.picture")
+    }
+
     return <div className={styles.overflow_background}>
         <div className={styles.points}>
             <div className={styles.points_content}>
                 <div className={styles.points_header}>
+                    <button className={styles.points_logout} onClick={logOut}>Log Out</button>
                     <div style={{margin: "auto"}}>
                         <h1 className={font.className}>Welcome, {name}!</h1>
                         <h2 className={font.className}>Your points balance: 0</h2>
